d18528d5823b0b3773fc5509c558ae625d29b619
unknown <jaredtiu1706@gmail.com>
2024-10-29 20:35:16 +0800

    trying out the count function

--- 1_to_50_input_finder.py
+++ 1_to_50_input_finder.py
@@ -11,17 +11,17 @@ def number_11_to_20 (numbers):
 def number_21_to_30 (numbers):
     for digit in numbers:
         if digit >= 21 or digit <= 30:
-            list_of_numbers_11_to_20[numbers]
+            list_of_numbers_21_to_30[numbers]
     return True
 def number_31_to_40 (numbers):
     for digit in numbers:
         if digit >= 31 or digit <= 40:
-            list_of_numbers_11_to_20[numbers]
+            list_of_numbers_31_to_40[numbers]
     return True
 def number_41_to_50 (numbers):
     for digit in numbers:
         if digit >= 41 or digit <= 50:
-            list_of_numbers_11_to_20[numbers]
+            list_of_numbers_41_to_50[numbers]
     return True
 
 #get info to a  list
@@ -42,13 +42,19 @@ while True:
             break
         except: 
             print("Only add numbers from 1 to 50")
-    
-    list_of_numbers = [numbers]
+        list_of_numbers_1_to_10 = [numbers]
+        list_of_numbers_11_to_20 = [numbers]
+        list_of_numbers_21_to_30 = [numbers]
+        list_of_numbers_31_to_40 = [numbers]
+        list_of_numbers_41_to_50 = [numbers]
+
+    count = list_of_numbers_1_to_10.count(1)
 
     retry = input("Do you want to input another number?: ")
 
     #if they stop inputing print how many inputs
     if retry == "no" or retry == "No" or retry == "No":
+        print([number_1_to_10] , count)
         break 
 
 #find a way to detect how many inputs for a number in that list 
