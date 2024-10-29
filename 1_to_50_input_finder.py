65be7f1826c7864cab9d4c074cf141f3ed3fee73
unknown <jaredtiu1706@gmail.com>
2024-10-29 19:30:22 +0800

    added pseudocode

---
+++ 1_to_50_input_finder.py
@@ -0,0 +1,10 @@
+#make a loop for user input 
+
+#add definitions for valid age 
+
+#get info to a  list 
+
+#find a way to detect how many inputs for a number in that list 
+
+#if they stop inputing print how many inputs 
+
