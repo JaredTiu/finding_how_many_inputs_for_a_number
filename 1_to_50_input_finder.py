5ab46069545b6ceb385eb2aa0af0e41a1abf3323
unknown <jaredtiu1706@gmail.com>
2024-10-29 19:55:32 +0800

    added loop asking for user input

--- 1_to_50_input_finder.py
+++ 1_to_50_input_finder.py
@@ -1,4 +1,18 @@
 #make a loop for user input 
+while True:
+    while True: 
+        try: 
+            age = int(input("input a number: "))
+            if age < 0 or age > 50:
+                raise
+            break
+        except: 
+            print("Only add numbers from 1 to 50")
+            
+    retry = input("Do you want to input another number?: ")
+
+    if retry == "no" or retry == "No" or retry == "No":
+        break
 
 #add definitions for valid age 
 
