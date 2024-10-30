8c0058fb3a105f9fd2694d3c9eb319484be5a88f
unknown <jaredtiu1706@gmail.com>
2024-10-30 13:19:57 +0800

    removed the separate lists

--- 1_to_50_input_finder.py
+++ 1_to_50_input_finder.py
@@ -1,35 +1,8 @@
-def number_1_to_10 (numbers):
-    for digit in numbers: 
-        if digit >= 1 or digit <= 10:
-            list_of_numbers_1_to_10[numbers]
-    return True
-def number_11_to_20 (numbers):
-    for digit in numbers:
-        if digit >= 11 or digit <= 20:
-            list_of_numbers_11_to_20[numbers]
-    return True     
-def number_21_to_30 (numbers):
-    for digit in numbers:
-        if digit >= 21 or digit <= 30:
-            list_of_numbers_21_to_30[numbers]
-    return True
-def number_31_to_40 (numbers):
-    for digit in numbers:
-        if digit >= 31 or digit <= 40:
-            list_of_numbers_31_to_40[numbers]
-    return True
-def number_41_to_50 (numbers):
-    for digit in numbers:
-        if digit >= 41 or digit <= 50:
-            list_of_numbers_41_to_50[numbers]
-    return True
+from collections import Counter
 
 #get info to a  list
-list_of_numbers_1_to_10 = []
-list_of_numbers_11_to_20 = []
-list_of_numbers_21_to_30 = []
-list_of_numbers_31_to_40 = []
-list_of_numbers_41_to_50 = []
+list_of_numbers = []
+one_to_ten = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 
 #make a loop for user input 
 while True:
@@ -42,19 +15,22 @@ while True:
             break
         except: 
             print("Only add numbers from 1 to 50")
-        list_of_numbers_1_to_10 = [numbers]
-        list_of_numbers_11_to_20 = [numbers]
-        list_of_numbers_21_to_30 = [numbers]
-        list_of_numbers_31_to_40 = [numbers]
-        list_of_numbers_41_to_50 = [numbers]
 
-    count = list_of_numbers_1_to_10.count(1)
+        list_of_numbers = [numbers]
+
 
     retry = input("Do you want to input another number?: ")
 
     #if they stop inputing print how many inputs
     if retry == "no" or retry == "No" or retry == "No":
-        print([number_1_to_10] , count)
+        #find a way to detect how many inputs for a number in that list
+        count_1_to_10 = 0 
+        for digit in list_of_numbers:
+            if digit == 1:
+                count_1_to_10 += 1
+        
+        print(count_1_to_10)
+
         break 
 
 #find a way to detect how many inputs for a number in that list 
