e90f44498380a1312bede42aac9388a0170dca2d
unknown <jaredtiu1706@gmail.com>
2024-10-30 14:38:11 +0800

    resorted to 5 separate lists using if statement

--- 1_to_50_input_finder.py
+++ 1_to_50_input_finder.py
@@ -1,8 +1,9 @@
-from collections import Counter
-
 #get info to a  list
-list_of_numbers = []
-one_to_ten = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+list_of_1_to_10 = []
+list_of_11_to_20 = []
+list_of_21_to_30 = []
+list_of_31_to_40 = []
+list_of_41_to_50 = []
 
 #make a loop for user input 
 while True:
@@ -10,29 +11,26 @@ while True:
         try: 
             #add definitions for valid age 
             numbers = int(input("input a number: "))
-            if numbers < 0 or numbers > 50:
+            if numbers >= 1 and numbers <=10:
+                list_of_1_to_10.append(numbers)
+            elif numbers >= 11 and numbers <=20:
+                list_of_11_to_20.append(numbers)
+            elif numbers >= 21 and numbers <= 30: 
+                list_of_21_to_30.append(numbers)
+            elif numbers >= 31 and numbers <= 40:
+                list_of_31_to_40.append(numbers)
+            elif numbers >=40 and numbers <= 50:
+                list_of_41_to_50.append(numbers)    
+            else:
                 raise
             break
         except: 
             print("Only add numbers from 1 to 50")
-
-        list_of_numbers = [numbers]
-
-
     retry = input("Do you want to input another number?: ")
 
     #if they stop inputing print how many inputs
     if retry == "no" or retry == "No" or retry == "No":
         #find a way to detect how many inputs for a number in that list
-        count_1_to_10 = 0 
-        for digit in list_of_numbers:
-            if digit == 1:
-                count_1_to_10 += 1
-        
-        print(count_1_to_10)
 
-        break 
-
-#find a way to detect how many inputs for a number in that list 
- 
+        break  
 
